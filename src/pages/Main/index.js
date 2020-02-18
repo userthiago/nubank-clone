7a8731ca663fbd2de65df59eec3e6c2fe0cdd754
Thiago Henrique Santos <thiago.santos@fcamara.com.br>
2020-02-18 20:12:22 -0300

    [ADD] Layout da página principal do app.

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,59 +1,47 @@
 import React from 'react';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import Header from '~/components/Header';
+import Menu from '~/components/Menu';
+import Tabs from '~/components/Tabs';
+
 import {
-  Text, Image, StyleSheet, Dimensions, ImageBackground, StatusBar,
-} from 'react-native';
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  Title,
+  Description,
+  CardFooter,
+  Annotation,
+} from './styles';
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    flex: 1,
-    paddingHorizontal: 20,
-  },
-  fileName: {
-    fontWeight: 'bold',
-    marginTop: 5,
-  },
-  instructions: {
-    color: '#DDD',
-    fontSize: 14,
-    marginTop: 20,
-    textAlign: 'center',
-  },
-  logo: {
-    height: Dimensions.get('window').height * 0.11,
-    marginVertical: Dimensions.get('window').height * 0.11,
-    width: Dimensions.get('window').height * 0.11 * (1950 / 662),
-  },
-  welcome: {
-    color: '#fff',
-    fontSize: 22,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
-
-const Main = () => (
-  <ImageBackground
-    source={{
-      uri: 'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/background.png',
-    }}
-    style={styles.container}
-    resizeMode="cover"
-  >
-    <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-    <Image
-      source={{
-        uri: 'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/rocketseat_logo.png',
-      }}
-      style={styles.logo}
-      resizeMode="contain"
-    />
-    <Text style={styles.welcome}>Bem-vindo ao Template Básico!</Text>
-    <Text style={styles.instructions}>Essa é a tela principal da sua aplicação =)</Text>
-    <Text style={styles.instructions}>Você pode editar a tela no arquivo:</Text>
-    <Text style={[styles.instructions, styles.fileName]}>src/pages/Main/index.js</Text>
-  </ImageBackground>
-);
-
-export default Main;
+export default function Main() {
+  return (
+    <Container>
+      <Header />
+      <Content>
+        <Menu />
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 250.451,58</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferência de R$ 300,00 recebida de Thiago Henrique Santos hoje
+              às 06:00h.
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+      <Tabs />
+    </Container>
+  );
+}
